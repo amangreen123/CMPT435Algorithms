74d0055bbf8171fe6cacccfe4818ccdc19505e18
Aaron <Aaron.Green1@marist.edu>
2020-10-02 02:17:50 -0400

    Merge sort is sorting almost all the words need to find out where to put compre function

--- Assignment 2/untitled/main.cpp
+++ Assignment 2/untitled/main.cpp
@@ -66,8 +66,63 @@ void InSort(){
     }
     cout<< "Insertion sort number of comparisons is " <<num_comparison<<"\n";
 }
+void Merge(vector<string>&mitems, int p,int q,int r){
+  int n1 = q - p + 1;
+  int n2 = r - q;
 
-//Msort(){}
+  vector<string> leftVector;
+  vector<string> rightVector;
+
+  leftVector.resize(n1 + 1);
+  rightVector.resize(n2 + 1);
+
+  for(int i = 0; i < n1; i++){
+      leftVector.at(i) = mitems.at(p + i);
+      //try just p + i if it doesnt work
+    }
+  for(int j = 0; j < n2; j++){
+       rightVector.at(j) = words.at(q+j+1);
+       //q + 1 + j
+    }
+  int i,j,k;
+  i = 0;
+  j = 0;
+  k = p;
+
+  while(i < n1 && j < n2){
+
+      if(leftVector.at(i) <= rightVector.at(j)) {
+          words[k] = leftVector.at(i);
+          i++;
+      }else{
+          words[k] = rightVector.at(j);
+          j++;
+      }
+      k++;
+  }
+
+  while(i < n1){
+      words[k] = leftVector[i];
+      i++;
+      k++;
+  }
+    while(j < n2){
+        words[k] = rightVector[j];
+        j++;
+        k++;
+    }
+}
+
+void Msort(vector<string>&mitems, int leftVect, int rightVect){
+    if(leftVect < rightVect){
+        int m = leftVect + (rightVect - leftVect) / 2;
+        Msort(words,leftVect,m);
+        Msort(words,m + 1, rightVect);
+
+        Merge(words,leftVect,m,rightVect);
+        num_comparison++;
+    }
+}
 
 int partiton(vector<string>&item, int low,int high){
     string pivot = words[high];
@@ -84,7 +139,6 @@ int partiton(vector<string>&item, int low,int high){
         num_comparison++;
     }
     swap(words[i + 1], words[high]);
-    cout<< "Quick sort number of comparisons is " <<num_comparison<<"\n";
     return i + 1;
 }
 
@@ -114,7 +168,10 @@ void Qsort(vector<string>&items,int low, int high){
             }
             //sSort();
             //InSort();
-          Qsort(words,0,words.size());
+            // Qsort(words,0,words.size());
+           // cout << "Quick sort number of comparisons is " <<num_comparison<<"\n";
+           Msort(words,0,words.size() - 1);
+            cout << "Merge sort number of comparisons is " <<num_comparison<<"\n";
             letterFile.close();
         }
 
